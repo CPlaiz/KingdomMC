d43ac811fe326809a4a4e2f6793be18873dd24e4
CPlaiz <treesarne@gmail.com>
2021-12-30 17:45:18 +0100

    initial commit

---
+++ src/main/java/de/cplaiz/kingdom/KingdomMC.java
@@ -0,0 +1,17 @@
+package de.cplaiz.kingdom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KingdomMC extends JavaPlugin {
+
+    private KingdomMC plugin;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
